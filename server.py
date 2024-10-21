dcc715584d67d60e018bb455513bc93fa48e36cb
hawkerd <dhawk064@gmail.com>
2024-10-20 19:20:48 -0500

    feat: clients can subscribe to topics, and the server will store this information feat: clients subscribing to nonexistent topics will receive an error

--- server.py
+++ server.py
@@ -1,6 +1,12 @@
 import socket
 import threading
 
+# dictionary to track subscribers for each topic
+topics = {
+    "WEATHER": [],
+    "NEWS": []
+}
+
 # initialize the server
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -12,21 +18,44 @@ server.listen()
 print("[STARTING] Server is listening on localhost:5555")
 
 # handle messages from each client
-def handle_client(conn, addr) :
+def handle_client(conn, addr):
     print(f"[HANDLING CLIENT] {addr}")
     connected = True
 
     while connected:
         try:
-            message = conn.recv(1024).decode('utf-8') #
+            message = conn.recv(1024).decode('utf-8')
             if message:
                 print(f"[RECEIVED] {message} from {addr}")
+
+                # extract the type and details of the message
+                parts = message.split(',')
+
+                if len(parts) == 3 and parts[1].strip() == "SUB":
+                    handle_sub(parts, conn)
+                
             else:
                 connected = False
         except:
             connected = False
 
     conn.close()
+
+# handle message format <NAME, SUB, SUBJECT>
+def handle_sub(parts, conn):
+    # extract details
+    client_name = parts[0].strip()
+    subject = parts[2].strip()
+
+    # add the client to the subscriber list if it exists
+    if subject in topics:
+        if conn not in topics[subject]:
+            topics[subject].append(conn)
+        conn.send(f"<SUB_ACK: Subscribed to {subject}>".encode('utf-8'))
+        print(f"[SUBSCRIPTION] {client_name} subscribed to {subject}")
+    else:
+        conn.send(f"<ERROR: Subscription Failed - Subject {subject} Not Found>".encode('utf-8'))
+        print(f"[ERROR] {client_name} tried to subscribe to non-existent subject: {subject}")
             
 
 # repeatedly create new threads for new connections
